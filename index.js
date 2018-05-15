8d538b07b70c72d6710ba158c01282db4899f74d
Shuqian Ying <sqying@archymeta.com>
2018-05-16 04:35:27 +0800

    modenize, sync-with other apps

--- index.js
+++ index.js
@@ -3,30 +3,30 @@
 const
     path = require('path'),
     os = require('os'),
-    colors = require('colors'),
+    chalk = require('chalk'),
     readline = require('readline'),
     app_register = require(__dirname + '/lib/app-register'),
     local_shell = require(__dirname + '/lib/local-shell');
 
-process.stdout.columns = 120;
+process.stdout.columns = 220;
 
-app_register.regist().then((run) => {
+app_register.regist().then(run => {
     if (!run) {
-        console.log('OK, the current app is registered with 1-NET client. It can be launched there. Have a good day!');
+        console.log('OK, the current app is registered with V-NET client. It can be launched there. Have a good day!');
         setTimeout(() => {
             process.exit(0);
         }, 500);
-        console.log()
+        console.log();
         return;
     }
 
-    process.on('SIGINT', function() {
+    process.on('SIGINT', () => {
         process.exit(0);
     });
 
-    console.log(colors.cyan(`************************************************************************************************************
+    console.log(chalk.cyan(`************************************************************************************************************
 
-                Welcome to 1-NET ssh client
+                Welcome to V-NET ssh client
                 
                 Current Host: ${os.hostname()}
                 Host Type: ${os.type()}
@@ -35,11 +35,11 @@ app_register.regist().then((run) => {
                 Current Time: ${(new Date()).toLocaleString()}
 
 ************************************************************************************************************
-` + colors.gray.bold('Please specify the following parameters:') + `
+` + chalk.gray.bold('Please specify the following parameters:') + `
 
 `));
 
-    process.on('uncaughtException', function(err) {
+    process.on('uncaughtException', err => {
         console.log(err);
         process.exit(1);
     })
